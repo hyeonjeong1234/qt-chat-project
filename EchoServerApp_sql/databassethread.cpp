bcde4410b00f6312eb2018b7f735a029a6041eca
hyeonjeong1234 <phj474123@gmail.com>
2025-06-19 23:42:46 +0900

    test merge

--- EchoServerApp_sql/databassethread.cpp
+++ EchoServerApp_sql/databassethread.cpp
@@ -76,7 +76,7 @@ DataBasseThread::DataBasseThread(QObject *obj)
                       << query.lastError().text().toStdString() << std::endl;
         }
 
-
+//test merge
         clientModel = new QSqlTableModel();
         clientModel->setTable("client");
         clientModel->setEditStrategy(QSqlTableModel::OnFieldChange);
